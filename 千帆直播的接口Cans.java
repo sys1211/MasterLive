f08987aa5122bc6d4dd9cb762207430d3d12dfba
sys <sys18635503918@outlook.com>
2017-06-01 10:11:34 +0800

    shiyuesong

---
+++ 千帆直播的接口Cans.java
@@ -0,0 +1,66 @@
+package com.example.ciscen.qianfan.cans;
+
+/**
+ * Created by YanZiAng on 16-7-18
+ */
+public class Cans {
+    //首页
+    public static final String HOME_PAGE = "http://qf.56.com/home/v4/moreAnchor.android?&type=%d&index=";
+    //直播间
+    public static final String LIVE_ROOM = "http://qf.56.com/play/v1/preLoading.android?roomId=";
+    //房主信息
+    public static final String ANCHORINFO = "http://qf.56.com/play/v4/enterShowRoom.android?roomId=";
+    public static final String ANDIENCEINFO = "http://qf.56.com/audience/chat/v2/getAudiences.android?pageNum=1&pageSize=20&roomId=";
+    //排行
+    //明星榜
+    public static final String RANK_STAR = "http://qf.56.com/rank/v1/rankStar.android?pageSize=30&type=";
+    //富豪榜
+    public static final String RANK_WEALTH = "http://qf.56.com/rank/v1/rankWealth.android?pageSize=30&type=";
+    //人气榜
+    public static final String RANK_POPULARITY = "http://qf.56.com/rank/v1/rankPopularity.android?pageSize=30&type=";
+    //周星榜 本周
+    public static final String RANK_FANS_THIS_WEEK = "http://qf.56.com/activity/star/v1/rankAll.android?ts=1468751974817&weekly=0&signature=2cc9bcaa7df99dd33019a04f30a699ce";
+    //周星榜 上周
+    public static final String RANK_FANS_LAST_WEEK = "http://qf.56.com/activity/star/v1/rankAll.android?weekly=1&ts=1468751119768&signature=1a24f68780b5115d13b11b2f55ae0273";
+
+    //发现
+    //顶部轮播
+    public static final String HEADER_LOOP = "http://qf.56.com/home/v4/getBanners.android";
+    //猜你喜欢
+    public static final String GUESS_YOUR_LIKE = "http://qf.56.com/home/v4/guess.android";
+    //商城 座驾
+    public static final String STORY_RIDE = "http://qf.56.com/pay/v1/goodsList.android?page=1&rows=100&ts=1468743707354&type=3&type2=0";
+}
+package com.example.ciscen.qianfan.cans;
+
+/**
+ *
+ */	
+public class Cans {                  
+    //首页    //http://qf.56.com/home/v4/moreAnchor.android?&type=0&index=0    注：type0-8;
+    public static final String HOME_PAGE = "http://qf.56.com/home/v4/moreAnchor.android?&type=%d&index=";
+    //直播间
+    public static final String LIVE_ROOM = "http://qf.56.com/play/v1/preLoading.android?roomId=";
+    //房主信息
+ public static final String ANCHORINFO = "http://qf.56.com/play/v4/enterShowRoom.android?roomId=";
+    public static final String ANDIENCEINFO = "http://qf.56.com/audience/chat/v2/getAudiences.android?pageNum=1&pageSize=20&roomId=";
+    //排行
+    //明星榜
+    public static final String RANK_STAR = "http://qf.56.com/rank/v1/rankStar.android?pageSize=30&type=";
+    //富豪榜
+    public static final String RANK_WEALTH = "http://qf.56.com/rank/v1/rankWealth.android?pageSize=30&type=";
+    //人气榜
+  public static final String RANK_POPULARITY = "http://qf.56.com/rank/v1/rankPopularity.android?pageSize=30&type=";
+    //周星榜 本周
+    public static final String RANK_FANS_THIS_WEEK = "http://qf.56.com/activity/star/v1/rankAll.android?ts=1468751974817&weekly=0&signature=2cc9bcaa7df99dd33019a04f30a699ce";
+    //周星榜 上周
+    public static final String RANK_FANS_LAST_WEEK = "http://qf.56.com/activity/star/v1/rankAll.android?weekly=1&ts=1468751119768&signature=1a24f68780b5115d13b11b2f55ae0273";
+
+    //发现
+    //顶部轮播
+    public static final String HEADER_LOOP = "http://qf.56.com/home/v4/getBanners.android";
+    //猜你喜欢
+    public static final String GUESS_YOUR_LIKE = "http://qf.56.com/home/v4/guess.android";
+    //商城 座驾
+    public static final String STORY_RIDE = "http://qf.56.com/pay/v1/goodsList.android?page=1&rows=100&ts=1468743707354&type=3&type2=0";
+}
